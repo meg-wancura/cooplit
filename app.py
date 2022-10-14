c07daf7c9891ab491a9a9aca1e592cfc30f40984
Megan Wancura <mwancura13@gmail.com>
2022-10-14 13:48:11 -0400

    Fully updating web progress

--- app.py
+++ app.py
@@ -4,12 +4,5 @@ app = Flask(__name__)
 app.secret_key = "password"
 
 @app.route("/")
-@app.route("/home")
-def home():
-    flash("Hello! Welcome to Cooplit")
-    return render_template("index.html")
-
-@app.route("/greet", methods=["POST","GET"])
-def greet():
-    flash("Hi "+ str(request.form['name_input']) + ", great to see you")
+def index():
     return render_template("index.html")
