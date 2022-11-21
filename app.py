0e152118ebea3ef0c4dc590b86bafb34a79f64dc
Megan Wancura <mwancura13@gmail.com>
2022-11-21 18:50:02 -0500

    current status 11.22.22

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "password"
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("../html/about")
+def about():
+    return "<h2>about test</h2>"
+
+    ##render_template("../html/about.html")###
+
+@app.route("../html/contact")
+def contact():
+    return render_template("../html/contact.html")
