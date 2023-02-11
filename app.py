7f3877d686cd7a2104777d59345426b637202b8c
meg-wancura <mwancura13@gmail.com>
2023-02-11 17:34:01 -0500

    small cahnges

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ app.secret_key = "password"
 def index():
     return render_template("/index.html")
 
-@app.route("/about")
-def about():
-    return render_template("/about.html")
-
 @app.route("/contact")
 def contact():
     return render_template("/contact.html")
