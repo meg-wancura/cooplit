97d1dc6f96ac3b238ceb39e884653cc4ca230c82
Megan Wancura <mwancura13@gmail.com>
2023-02-18 13:18:50 -0500

    Commit

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, flash
+import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = "password"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 @app.route("/")
@@ -13,5 +19,13 @@ def index():
 def contact():
     return render_template("/contact.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/mock")
+def contact():
+    return render_template("/mock.html")
+
+@app.route("/function")
+def function():
+    req = requests.get('https://api.openalex.org/authors/https://orcid.org/0000-0002-1298-3089')
+    data = req.content
+    json_data = json.loads(data)
+    return render_template('function.html', data=jason_data)
