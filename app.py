4bcbe771be7b9d4ed51514affc438466b3bf9660
Megan Wancura <mwancura13@gmail.com>
2023-02-18 16:01:49 -0500

    commitminor adjustment

--- app.py
+++ app.py
@@ -9,23 +9,26 @@ app.secret_key = "password"
 if __name__ == '__main__':
     app.run(debug=True)
 
-
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("/index.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET"])
 def contact():
     return render_template("/contact.html")
 
-@app.route("/mock")
+@app.route("/mock", methods=["GET"])
 def contact():
     return render_template("/mock.html")
 
-@app.route("/function")
+@app.route("function", methods=["GET"])
 def function():
     req = requests.get('https://api.openalex.org/authors/https://orcid.org/0000-0002-1298-3089')
     data = req.content
     json_data = json.loads(data)
     return render_template('function.html', data=jason_data)
+
+@app.errorhandler(404)
+def error():
+	return render_template("error.html")
